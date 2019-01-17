840bbb5bf4ebb8efe6bd836f48cbf38ef70e9f88
Martin Diko <mdiko@valorem.com>
2019-01-16 16:18:54 -0800

    Dikodev: First Angular - Material app - initial

--- README.md
+++
@@ -1,1 +0,0 @@
-# dikodev-angular-material
---
+++ src/app/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatAutocompleteModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatMenuModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatAutocompleteModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatAutocompleteModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule
+    ]
+})
+
+export class MaterialModule {
+
+}
